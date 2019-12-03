921e700eb019175d342b41d030fc3ed5e8a10313
VadimPeshko <peshko.vadzim@gmail.com>
2019-12-03 22:06:55 +0300

    fix(hw1): fixed some errors

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -1,92 +1,82 @@
 function getNumWord(num, word1, word2, word5) {
-    let dd = num % 100;
-    let oneDigit = num % 10;
-  
-    switch(true) {
-      case ((dd >= 11) && (dd <= 19)):
-        return word5;
-      case (oneDigit === 1):
-        return word1;
-      case (oneDigit >= 2 && oneDigit <= 4):
-        return word2;
-      default:
-        return word5;
-    }
-  };
-  
-  function makeTest() {
-    let applesCount = parseInt(prompt('Сколько яблок?'));
-    if (applesCount) {
-      alert('У вас ' + applesCount + ' ' + getNumWord(applesCount,
-          'яблоко', 'яблока', 'яблок'));
-    }
-  };
+  const dd = num % 100;
+  const oneDigit = num % 10;
 
-  function getData() {
-    let surname = prompt('Введите вашу фамилию', 'Иванов');
-    let name = prompt('Введите ваше имя', 'Иван');
-    let patronymic = prompt('Введите ваше отчество', 'Иванович');
-    let age = +prompt('Введите ваш возраст', '18');
-    let sex = confirm('Ваш пол - мужской?');
+  switch (true) {
+    case ((dd >= 11) && (dd <= 19)):
+      return word5;
+    case (oneDigit === 1):
+      return word1;
+    case (oneDigit >= 2 && oneDigit <= 4):
+      return word2;
+    default:
+      return word5;
+  }
+}
 
-    if (surname === '') {
-      surname = 'Иванов';
-    }
-    if (name === '') {
-      name = 'Иван';
-    }
-    if (patronymic === '') {
-      patronymic = 'Иванович';
-    }
-    if (age === 0) {
-      age = 18;
-    }
-    if (sex) {
-      sex = 'Мужской';
-    }
-    else {
-      sex = 'Женский';
-    }
+function makeTest() {
+  const applesCount = parseInt(prompt('Сколько яблок?'));
+  if (applesCount) {
+    alert(`У вас ${applesCount} ${getNumWord(applesCount,
+      'яблоко', 'яблока', 'яблок')}`);
+  }
+}
 
-    alert(`Ваше ФИО: ${surname} ${name} ${patronymic}
-    Ваш возраст в годах: ${age}
-    Ваш возраст в днях: ${age * 365}
-    Через 5 лет вам будет: ${age + 5}
-    Ваш пол: ${sex}
-    Вы на пенсии: ${age > 65 ? 'Да' : 'Нет'}`);
-};
-  
+function getData() {
+  let surname = prompt('Введите вашу фамилию', 'Иванов');
+  let name = prompt('Введите ваше имя', 'Иван');
+  let patronymic = prompt('Введите ваше отчество', 'Иванович');
+  let age = +prompt('Введите ваш возраст', '18');
+  const sex = confirm('Ваш пол - мужской?');
+
+  if (surname === '') {
+    surname = 'Иванов';
+  }
+  if (name === '') {
+    name = 'Иван';
+  }
+  if (patronymic === '') {
+    patronymic = 'Иванович';
+  }
+  if (age === 0) {
+    age = 18;
+  }
+
+  alert(`Ваше ФИО: ${surname} ${name} ${patronymic}
+  Ваш возраст в годах: ${age}
+  Ваш возраст в днях: ${age * 365}
+  Через 5 лет вам будет: ${age + 5}
+  Ваш пол: ${sex === true ? 'Мужской' : 'Женский'}
+  Вы на пенсии: ${((age > 63) && (sex === true)) ? 'Да' : ((age > 58) && (sex === false)) ? 'Да' : 'Нет'}`);
+}
+
 function isPolindrom(str) {
   let res = [];
-  let res1 = [];
-  let res2 = [];
+  const res1 = [];
+  const res2 = [];
 
-  for(let i = 0; i < str.length; i++){
-    res.push(str[i]);
-  }
-  for(let i = 0; i < Math.floor(res.length/2); i++){
+  res = str.split('');
+
+  for (let i = 0; i < Math.floor(res.length / 2); i++) {
     res1.push(str[i]);
   }
-  for(let i = Math.ceil(res.length/2); i < res.length; i++){
+  for (let i = Math.ceil(res.length / 2); i < res.length; i++) {
     res2.push(str[i]);
   }
 
   res2.reverse();
 
-  for(let i = 0; i < res1.length; i++){
-    if(res1[i] === res2[i]){
-      console.log('Полиндром');
-    }else {
-      console.log('Не полиндром');
-    }
+  for (let i = 0; i < res1.length; i++) {
+    return res1[i] === res2[i] ? console.log('Полиндром') : console.log('Не полиндром');
   }
-};
+}
 
 
 function getMultiplyTable() {
-  const body = document.body;
+  const { body } = document;
   const table = document.createElement('table');
-  let tr, td, tdInner = '';
+  let tr;
+  let td;
 
   table.style.width = '100%';
   table.setAttribute('border', '1');
@@ -107,7 +97,7 @@ function getMultiplyTable() {
 
   body.append(table);
 }
-makeTest();
-getData();
-isPolindrom("fooof");
-getMultiplyTable();
+// makeTest();
+// getData();
+isPolindrom('rooor');
+// getMultiplyTable();
